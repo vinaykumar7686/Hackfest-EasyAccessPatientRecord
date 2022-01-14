e3c1c74cbb63f74f997ab3bc15d145a011c4f367
coder9a <rajsinghbalhara@gmail.com>
2022-01-14 19:56:02 +0530

    doctor page

--- doctor/urls.py
+++ doctor/urls.py
@@ -1,3 +1,4 @@
+from pydoc import doc
 from django.urls import path
 
 from doctor.views import doc_homepage, login
@@ -8,9 +9,9 @@ urlpatterns = [
     path('', homepage, name='homepage'),
     path('login/', login, name = 'login'),
     path('doctor/', doc_homepage, name = 'doc_homepage'),
-    path('doctor/register/', doc_register, name = 'doc_register'),
-    
+    path('doctor/register/', doc_register, name = 'doc_register'),    
     path('doctor/prescription', view_prescription, name='view_prescription'),
-    path('prescription/add', add_prescription)
+    path('prescription/add', add_prescription),
+    path('alldoctors/',doc_info)
     
 ]
