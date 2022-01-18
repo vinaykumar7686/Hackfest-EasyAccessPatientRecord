21e7642b0c851b9dd5053ed64dcbd8e4a8cc93f7
Vinay Kumar <49321180+vinaykumar7686@users.noreply.github.com>
2022-01-18 12:56:54 +0530

    Update urls.py

--- patient/urls.py
+++ patient/urls.py
@@ -3,11 +3,17 @@ from .views import *
 
 
 urlpatterns = [
+    # url for patient's homepage
     path('', pat_homepage, name='pat_homepage'),
+    # url for patient's registration
     path('register/', pat_register),
+    # url for patient's medical form fill up
     path('medicalForm/', pat_medicalForm),
+    # url for patient's personal profile
     path('info/', pat_info),
+    # url for updating patient's personal medical info
     path('update/medicalinfo/', update_medicalinfo),
+    # url for patient's basic info
     path('update/info/', update_patient),
 ]
 
